ad8dd9d401c81530d75c75be92913932ed24a561
hope04302 <hope04302@gmail.com>
2024-06-17 02:34:25 +0900

    re commit

--- pages/analysis_1.py
+++ pages/analysis_1.py
@@ -3,12 +3,4 @@ from common_container import menu
 
 menu()
 
-st.markdown("""
-누군가는...
-```
-import streamlit as st
-
-for i in range(10):
-    pass
-```
-""")
+st.markdown()
---
+++ pages/analysis_6.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from common_container import menu
+
+menu()
