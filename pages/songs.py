598b102e501f9685ec9855a3fce63e613fd50b05
hope04302 <hope04302@gmail.com>
2024-06-17 13:56:09 +0900

    final commit

--- pages/songs.py
+++ pages/songs.py
@@ -12,6 +12,7 @@ cluster_id = st.session_state.get("cluster_id")
 if cluster_id is None:
     st.switch_page("app.py")
 
+st.session_state["show_lyrics"] = {}
 
 def jamo_contains(haystack, needle):
     haystack_jamo = j2hcj(h2j(haystack))
@@ -30,6 +31,27 @@ def search_songs_by(df, value, value_options, max_count=50):
         if len(song_indices) >= max_count:
             return df.loc[song_indices]
 
+@st.experimental_fragment
+def frag(idx, row):
+    with st.container(border=True):
+
+        st.write(f"**Name**: {row['name']}")
+        st.write(f"**Album**: {row['album']}")
+        st.write(f"**Release Date**: {row['year']}-{row['month']:02d}-{row['day']:02d}")
+        st.write(f"**Artist**: {row['artist']}")
+        st.write(f"**Genre**: {row['genre']}")
+        st.write(f"**Lyric Writer**: {row['lyric_writer']}")
+        st.write(f"**Composer**: {row['composer']}")
+        st.write(f"**Arranger**: {row['arranger']}")
+
+        # 가사 표시 버튼
+        btn = st.button(f"Show/Hide Lyrics for {row['name']}", key=f"button_{idx}")
+        if btn:
+            st.session_state.get('show_lyrics')[idx] = not st.session_state.get('show_lyrics').get(idx, 0)
+        if st.session_state.get('show_lyrics').get(idx) == 1:
+            st.text(f"**Lyrics**:\n{row['lyrics']}")
+            st.page_link(f"https://www.melon.com/song/detail.htm?songId={idx}", label="to Melon Music")
+
 
 with st.form(key=f"form_{cluster_id}"):
 
@@ -44,25 +66,5 @@ song_df_selected = search_songs_by(song_df, search, targ_options)
 
 if song_df_selected is not None:
     for idx, row in song_df_selected.iterrows():
+        frag(idx, row)
 
-        with st.container(border=True):
-
-            st.write(f"**Name**: {row['name']}")
-            st.write(f"**Album**: {row['album']}")
-            st.write(f"**Release Date**: {row['year']}-{row['month']:02d}-{row['day']:02d}")
-            st.write(f"**Artist**: {row['artist']}")
-            st.write(f"**Genre**: {row['genre']}")
-            st.write(f"**Lyric Writer**: {row['lyric_writer']}")
-            st.write(f"**Composer**: {row['composer']}")
-            st.write(f"**Arranger**: {row['arranger']}")
-
-            # 가사 표시 버튼
-            if st.button(f"Show/Hide Lyrics for {row['name']}", key=f"button_{idx}"):
-                st.session_state['show_lyrics'] = idx if st.session_state.get('show_lyrics') != idx else -1
-                if idx != -1 and idx is not None:
-                    st.session_state[f"cont_{idx}"].rerun()
-
-            # 가사 표시
-            if st.session_state.get('show_lyrics') == idx:
-                st.write(f"**Lyrics**:\n{row['lyrics']}")
-                st.page_link(f"https://www.melon.com/song/detail.htm?songId={idx}", label="to Melon Music")
